ba6989ae48e260be1cc3e0f3339d713d4d41aca9
Ahmed <fullstackdeveloper404@gmail.com>
2025-01-26 20:46:20 +0530

    Added constraints in Enquiry form and Feedback form.

--- users/forms.py
+++ users/forms.py
@@ -2,26 +2,52 @@ from django import forms
 from .models import *
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.core.exceptions import ValidationError
 
 class FeedbackForm(forms.ModelForm):
     studentName = forms.CharField(label='Student Name', 
                     widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
-    studentGrade = forms.IntegerField(label='Student Grade [7 to 10]', 
-                    widget=forms.TextInput(attrs={'placeholder': 'Your Grade'}))
-    noOfStars = forms.IntegerField(label='Ratings [1 to 5]', 
-                    widget=forms.TextInput(attrs={'placeholder': 'Your Ratings'}))
-    review = forms.Textarea()           
+    
+    # Validate studentGrade to be between 7 and 10
+    studentGrade = forms.IntegerField(
+        label='Student Grade [7 to 10]', 
+        widget=forms.TextInput(attrs={'placeholder': 'Your Grade'})
+    )
+    
+    # Validate noOfStars to be between 0 and 5
+    noOfStars = forms.IntegerField(
+        label='Ratings [1 to 5]', 
+        widget=forms.TextInput(attrs={'placeholder': 'Your Ratings'})
+    )
+
+    review = forms.Textarea()
     feedback = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
     class Meta:
         model = Feedback
         fields = '__all__'
+
+    def clean_studentGrade(self):
+        grade = self.cleaned_data.get('studentGrade')
+        if grade < 7 or grade > 10:
+            raise ValidationError('Student Grade must be between 7 and 10.')
+        return grade
+
+    def clean_noOfStars(self):
+        stars = self.cleaned_data.get('noOfStars')
+        if stars < 1 or stars > 5:
+            raise ValidationError('Ratings must be between 1 and 5.')
+        return stars
        
 class EnquiryForm(forms.ModelForm):
     studentName = forms.CharField(label='Student Name', 
                     widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
-    phoneNumber = forms.IntegerField(label='Phone Number', 
-                    widget=forms.TextInput(attrs={'placeholder': 'Your Number'}))
+    
+    phoneNumber = forms.IntegerField(
+        label='Phone Number', 
+        widget=forms.TextInput(attrs={'placeholder': 'Your Number'})
+    )
+    
     subject = forms.CharField(label='Subject', 
                     widget=forms.TextInput(attrs={'placeholder': 'Your Subject'}))
     message = forms.Textarea()
@@ -30,10 +56,15 @@ class EnquiryForm(forms.ModelForm):
     class Meta:
         model = Enquiry
         fields = '__all__'
-        labels = {
-            "studentName": _("Student Name"),
-            "phoneNumber": _("Phone Number"),
-        }
+
+    def clean_phoneNumber(self):
+        phone_number = str(self.cleaned_data.get('phoneNumber'))
+        
+        if len(phone_number) != 10:
+            raise forms.ValidationError("Phone number must be exactly 10 digits long.")
+        
+        return phone_number
+
         
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
